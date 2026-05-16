bee68b98031c58bc39c7903509b1e196331482ac
Lord-Shad0w <kunal3270@gmail.com>
2026-05-16 18:14:27 +0530

    episode-02

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
-const heading1 = React.createElement("h1", { id: "head1" }, "Hello World!");
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const heading2 = React.createElement("h2", { id: "head2" }, "Namaste React!");
+
+const heading1 = (i) => React.createElement("h1", { id: "head1" + i }, "Hello World!");
+
+const heading2 = (i) => React.createElement("h2", { id: "head2" + i }, "Namaste React!");
 
 const div1 = React.createElement("div", { id: "container1" }, [
-  heading1,
-  heading2,
+  heading1(1),
+  heading2(1),
 ]);
 
 const div2 = React.createElement("div", { id: "container2" }, [
-  heading1,
-  heading2,
+  heading1(2),
+  heading2(2),
 ]);
 
 const heading = React.createElement("div", { id: "parent" }, [div1, div2]);
